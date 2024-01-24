5ddffbac1fa46bea90aa90824ed1a60c45245fbb
Arshad Qadri <arshad.qadri@caprium.com>
2024-01-24 10:31:40 +0530

    loader added

--- src/App.js
+++ src/App.js
@@ -1,33 +1,22 @@
 import "./App.css";
 import Debouncing from "./pages/Debouncing";
 import Home from "./pages/Home";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link  } from "react-router-dom";
 import Throttling from "./pages/Throttling";
 import TicTacToe from "./pages/TicTacToe";
 import LoginWithPhoneAndOTP from "./pages/LoginWithPhoneAndOTP";
 import Loader from "./pages/Loader";
 
+
 function App() {
-  // const arr = [9,4,7,65,12,56,0,0,65,9,99,66,3,76]
-  // const sorted = arr.sort((a,b)=>b-a)
-  // console.log(sorted);
-  // const avg = arr.reduce((a,b)=>a+b) / arr.length
-  // console.log(avg); 
-  // const removeDuplicates = new Set(arr)
-  // console.log(removeDuplicates);
-  // const newArray = [1,2,3,4,5,6,7,8]
-  // newArray.shift()
-  // newArray.unshift(9) 
-  // console.log( newArray.slice(5));
-  // console.log(newArray.splice(0,3));
-  // console.log(Array.isArray(newArray));
-  // console.log(newArray.pop());
+
   return (
     <div className="App">
       <div onClick={()=>{window.location.href="/"}} className="cursor-pointer w-full bg-black text-white py-3 font-bold text-2xl text-center">
         Practice Javascript
       </div>
-      <div className="p-5">
+      <Link to={"/"}><button className="bg-gray-800 text-white py-2 px-4 m-4 rounded-lg font-semibold">Back</button></Link>
+      <div className="p-5 h-[50vh]">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/debounce" element={<Debouncing />} />
--- src/pages/Loader.js
+++ src/pages/Loader.js
@@ -2,15 +2,25 @@ import React from "react";
 
 const Loader = () => {
   return (
-    <div className="h-[80vh] flex justify-center items-center flex-col">
-      <div className="bars w-[60px] h-8  relative">
-        <div className="w-1/5 absolute bottom-0  border-[2.5px] border-white  left-0 bg-black"></div>
-        <div className="w-1/5 absolute bottom-0  border-[2.5px] border-white  left-[20%] bg-black"></div>
-        <div className="w-1/5 absolute bottom-0  border-[2.5px] border-white  left-[40%] bg-black"></div>
-        <div className="w-1/5 absolute bottom-0  border-[2.5px] border-white  left-[60%] bg-black"></div>
-        <div className="w-1/5 absolute bottom-0  border-[2.5px] border-white  left-[80%] bg-black"></div>
+    <div className=" flex justify-center items-center gap-10 flex-wrap">
+      {/* loader 1 */}
+      <div>
+        <div className="bars w-[60px] h-8  relative">
+          <div className="w-1/5 absolute bottom-0  border-[2.5px] border-white  left-0 bg-black"></div>
+          <div className="w-1/5 absolute bottom-0  border-[2.5px] border-white  left-[20%] bg-black"></div>
+          <div className="w-1/5 absolute bottom-0  border-[2.5px] border-white  left-[40%] bg-black"></div>
+          <div className="w-1/5 absolute bottom-0  border-[2.5px] border-white  left-[60%] bg-black"></div>
+          <div className="w-1/5 absolute bottom-0  border-[2.5px] border-white  left-[80%] bg-black"></div>
+        </div>
+        <div className="text-sm ">Loading...</div>
       </div>
-      <div className="text-sm ">Loading...</div>
+      {/* loader 2 */}
+      <div>
+        <div className="w-10 h-10 rounded-full mx-4 border-4 border-black border-b-white animate-spin"></div>
+      </div>
+      <div>
+        <div className="loader"></div>
+      </div>
     </div>
   );
 };
